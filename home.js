80a6f097782d8e2fc9480e6bbb7c033d974a0420
Dipu657 <dipusaha657@gmail.com>
2023-09-19 09:17:20 +0100

    first commit

---
+++ home.js
@@ -0,0 +1,68 @@
+var name = 'dipu';
+var age = 22;
+console.log(age);
+let frist_name='Dipu';
+let Last_name='saha';
+full_name = frist_name+ " " +Last_name ;
+console.log('fullname='.fullname);
+ 
+let num1 = 20;
+let num2 =30;
+let num3= 25;
+Sum = num1+num2+num3
+subtractionvalue=num3-num1;
+multicationresult=num1*num2;
+
+console.log('total=', Sum);
+console.log('subtraction= ',subtractionvalue);
+console.log('multicationresult ',multicationresult);
+let remainer= num1 % num2;
+console.log('remainer=',remainer);
+let student1= 'riyad';
+let student2 =' sojal';
+const student3= 'joy';
+student2='himal';
+console.log(student2);
+console.log(student1);
+console.log(5<4);
+console.log(5>3);
+console.log(5<=4);
+console.log(5<=5);
+console.log(num1<num3 && num1==num3);
+
+let math_subject_marK= 80;
+let bangla_subject_marKeglish_subject_marK= 70;
+let bangla_subject_marK= 60;
+math_subject_marK+=10;
+console.log('Math=', math_subject_marK);
+let counter =0;
+counter++;
+counter--;
+counter++;
+console.log('counter=',counter);
+ let  value1= 20;
+ let value2 ='10';
+ let value2_convert_in_Number =parseInt(value2);
+ let total=value1+value2;
+ console.log(total);
+ console.log(value1);
+ let a= 0.2;
+ let b= 0.3;
+
+ let sum1 =a+b;
+ console.log(sum1);
+ let convertIn = sum1.toFixed(1)
+ 
+ let convert_in_Number = parseInt(convertIn)
+ console.log('convert in number ', convert_in_Number);
+ 
+
+ let password=15647820;
+ let user= "sahaariyan@gmail.com"
+ 
+ if (15647820==password && "sahaariyan@gmail.com"==user){
+    console.log('showing your profile');
+ }
+else{
+    console.log('password wrong');
+}
